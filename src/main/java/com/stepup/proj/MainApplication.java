591522358b6942c1afe0190c360184f0215c9cac
DAR-QA <blackskillow@gmail.com>
2024-07-07 14:16:53 +0300

    Улучшили логику main, пять

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -1,5 +1,7 @@
 package com.stepup.proj;
 
+import java.sql.SQLOutput;
+
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("3123213");
@@ -7,5 +9,6 @@ public class MainApplication {
         System.out.println("MAMA KAZA");
         System.out.println("третий коммит");
         System.out.println("четверочка");
+        System.out.println("пятый");
     }
 }
