9d451a5f6c8ce61d710078829c57a5f82c6a5a71
DAR-QA <blackskillow@gmail.com>
2024-07-07 13:58:45 +0300

    Улучшили логику main2

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,7 +2,8 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("3123213");
+        System.out.println("Hello, World2");
         System.out.println("MAMA KAZA");
         System.out.println("третий коммит");
     }
